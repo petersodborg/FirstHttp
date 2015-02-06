ef4020c793224ee25ee3fc4be219d8d2ef6a875c
petersodborg <sodborg@hotmail.com>
2015-02-06 08:02:44 +0100

    first commit

--- src/firsthttpserver/FirstHtttpServer.java
+++ src/firsthttpserver/FirstHtttpServer.java
@@ -3,9 +3,14 @@ package firsthttpserver;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 /**
  * @author Lars Mortensen
@@ -13,13 +18,15 @@ import java.net.InetSocketAddress;
 public class FirstHtttpServer {
   static int port = 8080;
   static String ip = "127.0.0.1";
+  static String contentFolder ="public/";
   public static void main(String[] args) throws Exception {
     if(args.length == 2){
       port = Integer.parseInt(args[0]);
-      ip = args[0];
+      ip = args[1];
     }
     HttpServer server = HttpServer.create(new InetSocketAddress(ip,port), 0);
     server.createContext("/welcome", new RequestHandler());
+    server.createContext("/files", new SimpleFileHandler());
     server.setExecutor(null); // Use the default executor
     server.start();
     System.out.println("Server started, listening on port: "+port);
@@ -28,11 +35,44 @@ public class FirstHtttpServer {
   static class RequestHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange he) throws IOException {
-      String response = "Welcome to my very first almost home made Web Server :-)";
+      String response = "<h1>Welcome to my very first almost home made Web Server :-)</h1>";
+      response += "<br>"+"URI:"+he.getRequestURI();
+      
+      
+      
+      Scanner scan = new Scanner(he.getRequestBody());
+      while(scan.hasNext()){
+          response += "<br/>" + scan.nextLine();
+      }
+      
+      
       he.sendResponseHeaders(200, response.length());
       try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
         pw.print(response); //What happens if we use a println instead of print --> Explain
       }
+      
     }
   }
+  
+  
+  
+  static class SimpleFileHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+      File file = new File(contentFolder + "index.html");
+      byte[] bytesToSend = new byte[(int) file.length()];
+      try {
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        bis.read(bytesToSend, 0, bytesToSend.length);
+      } catch (IOException ie) {
+        System.out.println(ie);
+      }
+      he.sendResponseHeaders(200, bytesToSend.length);
+      try (OutputStream os = he.getResponseBody()) {
+        os.write(bytesToSend, 0, bytesToSend.length);
+      }
+    }
+  }
+  
+  
 }
